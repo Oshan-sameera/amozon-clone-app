8e6243525b01260c6ae406d23cfa112030ee79b6
oshan-sameera <oshan951@gmail.com>
2021-12-19 15:52:04 +1100

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello Amazon clone!</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <nav className="header"></nav>;
+}
+
+export default Header;
