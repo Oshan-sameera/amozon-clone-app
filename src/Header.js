13f2c1bda5301b8aad8327cfc7f3cfd9b5fe6bc6
oshan-sameera <oshan951@gmail.com>
2021-12-19 16:02:12 +1100

    logo adeded

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Header() {
-  return <nav className="header"></nav>;
+  return (
+    <nav className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="logo"
+      />
+      <div className="header__option">
+        <span className="header__optionLineOne">Hello, User</span>
+        <span className="header__optionLineTwo">Sign In or Sing Out</span>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
