5ae577a06822eeddb855ff6b5cdd8ef79f744605
oshan-sameera <oshan951@gmail.com>
2021-12-20 13:43:19 +1100

    added home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
