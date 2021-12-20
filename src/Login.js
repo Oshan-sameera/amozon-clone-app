292146a3889db0a1cce274cb5aa74fd1246ccd26
oshan-sameera <oshan951@gmail.com>
2021-12-20 14:40:41 +1100

    created login paged

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Login.css";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <div className="login">
+      <Link>
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
       <div className="login__container">
         <h1>Sign In</h1>
         <form>
@@ -15,6 +23,9 @@ const Login = () => {
           </button>
         </form>
         <p>By Signing_in, you agree to Amozon's Term and Condtions</p>
+        <button className="login__registerButton">
+          Create Your Amazon Account
+        </button>
       </div>
     </div>
   );
